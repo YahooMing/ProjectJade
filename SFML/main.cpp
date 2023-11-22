62bd5c3811cb6dbc74f8f8c809a77d1ec446e7f9
Oskar <147434278+YahooMing@users.noreply.github.com>
2023-11-22 15:07:25 +0100

    Add files via upload

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -4,6 +4,35 @@
 #include "enemy.h"
 //#include "hero.h" //do zrobienia w przyszłości
 
+enum GameState{
+    MainMenu,
+    Gameplay
+};
+
+class Collectible {
+public:
+    sf::CircleShape shape;
+    bool isCollected;
+
+    Collectible(float x, float y) {
+        shape.setRadius(20);
+        shape.setFillColor(sf::Color::Yellow);
+        shape.setPosition(x, y);
+        isCollected = false;
+    }
+
+    void draw(sf::RenderWindow& window) {
+        if (!isCollected) {
+            window.draw(shape);
+        }
+    }
+
+    bool isColliding(const sf::RectangleShape& other) const {
+        return shape.getGlobalBounds().intersects(other.getGlobalBounds());
+    }
+};
+
+
 int main()
 {
     sf::VideoMode rozdzielczosc = sf::VideoMode::getDesktopMode(); //rozdzielczość
@@ -14,8 +43,8 @@ int main()
     std::vector<Platform> platforms;
     sf::View camera(sf::FloatRect(0, 0, rozdzielczosc.width, rozdzielczosc.height)); //stworzenie kamery
     window.setFramerateLimit(420); //ograniczenie fps do 420
-
-
+    sf::Clock clock;
+    sf::Time elapsed;
     hero.setFillColor(sf::Color::Red);
 
     sf::Vector2f PozycjaStartowa(600,350);
@@ -23,7 +52,7 @@ int main()
     hero.setPosition(PozycjaStartowa);
     hero.setSize(sf::Vector2f(100, 100));
 
-    floor.setSize(sf::Vector2f(1920, 40)); 
+    floor.setSize(sf::Vector2f(10000, 40)); 
     floor.setFillColor(sf::Color::Green); 
 
     std::vector<Enemy> enemies;
@@ -40,6 +69,10 @@ int main()
     int points = 1000;
 
 
+    sf::Clock damageClock;
+    const float damageCooldown = 0.5f;
+
+
     sf::Clock jumpClock;
     const float jumpCooldown = 0.7f;
 
@@ -47,6 +80,14 @@ int main()
 
     platforms.push_back(Platform(100, 600, 200, 40));
     platforms.push_back(Platform(400, 500, 200, 40));
+
+    std::vector<Collectible> collectibles;
+
+    // W miejscu, gdzie inicjalizujesz inne obiekty, dodaj kilka znajdźek:
+    collectibles.push_back(Collectible(200, groundHeight - 100));
+    collectibles.push_back(Collectible(600, groundHeight - 200));
+    // Dodaj tyle, ile chcesz znajdźek.
+
 
     enemies.push_back(Enemy(1000, (groundHeight-80), 80, 80, 1.0f, 1000, 1500)); 
 
@@ -65,14 +106,55 @@ int main()
     pointsText.setCharacterSize(24);
     pointsText.setFillColor(sf::Color::White);
 
+    sf::Text startText;
+    startText.setFont(font);
+    startText.setCharacterSize(50);
+    startText.setFillColor(sf::Color::White);
+    startText.setString("Press SPACE to start");
+
+    sf::Text endText;
+    endText.setFont(font);
+    endText.setCharacterSize(50);
+    endText.setFillColor(sf::Color::White);
+    endText.setString("GAME OVER");
+
+    GameState gameState = MainMenu;
+    bool isInMainMenu = true;
+    bool isGameOver = false;
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                window.close(); 
+            if(event.type == sf::Event::KeyPressed){
+                if(event.key.code == sf::Keyboard::Enter){
+                    if(gameState == MainMenu){
+                        gameState = Gameplay;
+                        isInMainMenu = false;
+                    }else if(isGameOver){
+                        gameState = MainMenu;
+                        isGameOver = false;
+                        isInMainMenu = true;
+                    }
+                }
+            }
         }
+       
+        if(gameState == MainMenu){
+            window.clear();
+            window.draw(startText);
+            window.display();
+        }
+        else if (gameState == Gameplay){
+            if(isGameOver){
+                window.clear();
+                window.draw(endText);
+                window.display();
+            }
+            elapsed = clock.restart();
         camera.setCenter(hero.getPosition().x , camera.getCenter().y); //ustawienie kamery na bohaterze
         window.setView(camera); //ustawienie widoku na kamerze
         sf::Vector2f cameraPosition = camera.getCenter();
@@ -137,18 +219,6 @@ int main()
             hero.setPosition(hero.getPosition().x, groundHeight - hero.getSize().y);
         }
 
-      
-        for (const auto& enemy : enemies)
-        {
-            if (enemy.isColliding(hero))
-            {
-            //window.close();
-            //std::cout << enemy.getSpeed() << std::endl;
-            //if (hero.getHealth() > 0) {
-            //hero.takeDamage(1); // Odejmowanie życia bohaterowi (możesz zmienić ilość)
-            }
-        }
-
 
         window.clear();
         window.draw(hero);
@@ -156,11 +226,38 @@ int main()
         window.draw(healthText);
         window.draw(pointsText);
 
-        for (auto& enemy : enemies)
-        {
-            enemy.patrol();
-            enemy.draw(window);
+// Sprawdzenie kolizji z wrogami
+for (auto it = enemies.begin(); it != enemies.end(); ) {
+    it->patrol();
+    it->draw(window);
+
+    // Sprawdzenie kolizji z wrogiem
+    if (it->isColliding(hero)) {
+        if (it->isHeadHit(hero) && isJumping) {
+            // Kolizja z głową wroga podczas skoku - usuń wroga i dodaj punkty
+            it = enemies.erase(it);
+            points += 100; // Przykładowo dodaje 100 punktów po zniszczeniu wroga
+        } else {
+            // Kolizja bez uderzenia w głowę - odejmij życie
+            if (damageClock.getElapsedTime().asSeconds() >= damageCooldown) {
+                health--;
+                damageClock.restart();
+            }
         }
+    } else {
+        ++it;
+    }
+}
+
+
+        for (auto it = collectibles.begin(); it != collectibles.end();) {
+        if (it->isColliding(hero) && !it->isCollected) {
+        points += 50;  // Przykładowo przyznaj 50 punktów za zebranie każdej znajdźki
+        it->isCollected = true;
+    }
+    it->draw(window);
+    ++it;
+}
 
         //window.draw(platform);     
         for (const auto& platform : platforms)
@@ -168,6 +265,8 @@ int main()
             platform.draw(window);
         }
         window.display();
+        //sf::sleep(sf::seconds(1.f/60.f));
+        }
    
     }
 
