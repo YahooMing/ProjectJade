b9026e8e4ec916791d87c8f1faf07f3979301a59
Oskar <147434278+YahooMing@users.noreply.github.com>
2023-12-06 10:55:13 +0100

    Add files via upload

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "platform.h"
 #include <iostream>
 #include "enemy.h"
@@ -10,6 +11,11 @@ enum GameState{
     Gameplay,
     LevelSelection,
     Exit
+};
+
+enum ScreenMode {
+    Windowed,
+    Fullscreen
 };
 
 class MenuItem {
@@ -25,6 +31,7 @@ public:
         text.setPosition(position);
     }
 };
+
 
 class Collectible {
 public:
@@ -52,8 +59,8 @@ public:
 
 int main()
 {
+    ScreenMode screenMode = Windowed;
     sf::VideoMode rozdzielczosc = sf::VideoMode::getDesktopMode(); //rozdzielczość
-
     sf::RenderWindow window(rozdzielczosc, "Project Jade");
     sf::RectangleShape hero;
     sf::RectangleShape floor;
@@ -89,7 +96,6 @@ int main()
 
     sf::Clock damageClock;
     const float damageCooldown = 0.5f;
-
 
     sf::Clock jumpClock;
     const float jumpCooldown = 0.7f;
@@ -134,11 +140,52 @@ int main()
     exitText.setFont(font);
     exitText.setCharacterSize(50);
     exitText.setFillColor(sf::Color::White);
-    exitText.setString("WYJDZ (ESC)");
+    exitText.setString("EXIT (ESC)");
 
+    sf::Text levelText;  // Tekst do wyświetlania informacji o wyborze poziomu
+    levelText.setFont(font);
+    levelText.setCharacterSize(24);
+    levelText.setFillColor(sf::Color::White);
+    levelText.setString("Level Selection:\n1. Easy\n2. Medium\n3. Hard");
+    levelText.setPosition(window.getSize().x / 2.0f - levelText.getLocalBounds().width / 2.0f,
+                          window.getSize().y / 2.0f - levelText.getLocalBounds().height / 2.0f);
+
+
+    sf::Text menuText;
+    menuText.setFont(font);
+    menuText.setCharacterSize(24);
+    menuText.setFillColor(sf::Color::White);
+    menuText.setString("PRESS 'P' TO CHANGE SCREEN MODE");
+    float xPositionMenuText = window.getSize().x - menuText.getLocalBounds().width - 20.0f;
+    float yPositionMenuText = window.getSize().y - menuText.getLocalBounds().height - 20.0f;
+    menuText.setPosition(xPositionMenuText, yPositionMenuText);
+    //sf::Music music1;
+
+
+
+    //if (!music1.openFromFile("music1.wav"))
+    //{
+        //return -1;
+    //}
     sf::Texture mcTexture;
     if (!mcTexture.loadFromFile("mc.png")) {
         return -1;
+    }
+    std::vector<sf::Texture> mcRight;
+    std::vector<sf::Texture> mcLeft;
+    const int numFrames =2;
+    for(int i=0;i<numFrames;++i){
+        sf::Texture texRight;
+        sf::Texture texLeft;
+
+        std::string filenameRight = "right_" + std::to_string(i) + ".png";
+        std::string filenameLeft = "left_" + std::to_string(i) + ".png";
+
+        if(!texRight.loadFromFile(filenameRight) || !texLeft.loadFromFile(filenameLeft)){
+            return -1;
+        }
+        mcRight.push_back(texRight);
+        mcLeft.push_back(texLeft);
     }
     sf::Texture enemyTexture;
     if (!enemyTexture.loadFromFile("enemy.png")) {
@@ -149,6 +196,15 @@ int main()
     // Utwórz obiekt sprite
     sf::Sprite MC(mcTexture);
     MC.setScale(5, 5); // Ustaw skalę sprite'a
+
+    //sf::Sprite MCRight(mcRight);
+    //MCRight.setScale(5, 5);
+    bool  isRight = false;
+    bool isLeft = false;
+    int currentFrame = 0;
+    sf::Clock animationClock;
+    const float animationSpeed = 0.1f;
+    sf::Sprite currentMC = MC; // Ustaw sprite'a jako aktualny sprite
 
     sf::Sprite enemy(enemyTexture);
     enemy.setScale(4, 4);
@@ -178,21 +234,34 @@ int main()
                 window.close(); 
             if(event.type == sf::Event::KeyPressed){
                 if(event.key.code == sf::Keyboard::Enter){
-                    if(gameState == MainMenu){
-                        gameState = Gameplay;
-                        isInMainMenu = false;
+                    if(gameState == MainMenu){                            
+                            gameState = LevelSelection;
+                            isInMainMenu = false;
                     }else if(isGameOver){
                         gameState = MainMenu;
                         isGameOver = false;
                         isInMainMenu = true;
+                    }else if(gameState == LevelSelection){
+                        gameState = Gameplay;
+                        isGameOver = false;
+                        isInMainMenu = false;
                     }
                 }else if(event.key.code == sf::Keyboard::Escape){
                     if(gameState == MainMenu){
                         window.close();
                     }
                 }else if (event.key.code == sf::Keyboard::Tab && gameState == Gameplay ){
-                isGamePaused = !isGamePaused;
-            }
+                    isGamePaused = !isGamePaused;
+                }else if (event.key.code == sf::Keyboard::P && gameState == MainMenu) {
+                    // Zmiana trybu ekranu po naciśnięciu P w menu
+                    if (screenMode == Windowed) {
+                        screenMode = Fullscreen;
+                        window.create(rozdzielczosc, "Project Jade", sf::Style::Fullscreen);
+                    } else {
+                        screenMode = Windowed;
+                        window.create(rozdzielczosc, "Project Jade", sf::Style::Default);
+                    }
+                }
             }
         }
        
@@ -200,14 +269,20 @@ int main()
             window.clear();
             window.draw(startText);
             window.draw(exitText);
+            window.draw(menuText);
             window.display();
-        }
-        else if (gameState == Gameplay && !isGamePaused){
+        }else if(gameState == LevelSelection){
+            window.clear();
+            window.draw(levelText);
+            window.display();
+        }else if (gameState == Gameplay && !isGamePaused){
             if(isGameOver){
                 window.clear();
                 window.draw(endText);
                 window.display();
             }
+            //music1.play();
+            //music1.setLoop(true);
             elapsed = clock.restart();
         camera.setCenter(hero.getPosition().x , camera.getCenter().y); //ustawienie kamery na bohaterze
         window.setView(camera); //ustawienie widoku na kamerze
@@ -236,10 +311,17 @@ int main()
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             hero.move(-xVelocity, 0);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            isLeft = true;
+            isRight = false;
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
             hero.move(xVelocity, 0);
+            isRight = true;
+            isLeft = false;
+        } else {
+            isRight = false;
+            isLeft = false;
+            currentFrame = 0;
         }
         
         // Skakanie
@@ -272,14 +354,32 @@ int main()
             xVelocity = 1;
             hero.setPosition(hero.getPosition().x, groundHeight - hero.getSize().y);
         }
-        MC.setPosition(hero.getPosition().x, hero.getPosition().y-10); // Ustaw pozycję sprite'a na pozycji bohatera
+
+if (isRight || isLeft)
+{
+    if (animationClock.getElapsedTime().asSeconds() >= animationSpeed)
+    {
+        currentFrame = (currentFrame + 1) % numFrames;
+        animationClock.restart();
+    }
+}
+
+if (isRight)
+{
+    currentMC.setTexture(mcRight[currentFrame]);
+}
+else if (isLeft)
+{
+    currentMC.setTexture(mcLeft[currentFrame]);
+}
+        currentMC.setPosition(hero.getPosition().x, hero.getPosition().y-10); // Ustaw pozycję sprite'a na pozycji bohatera
         enemy.setPosition(enemies[0].getPosition().x, enemies[0].getPosition().y-10);
 
 
         window.clear();
         window.draw(hero);
         window.draw(enemy);
-        window.draw(MC);
+        window.draw(currentMC);
         window.draw(floor);
         window.draw(healthText);
         window.draw(pointsText);
@@ -328,6 +428,7 @@ for (auto it = enemies.begin(); it != enemies.end(); ) {
         window.display();
         //sf::sleep(sf::seconds(1.f/60.f));
         }else if (isGamePaused)  // Dodaj warunek sprawdzający, czy gra jest zatrzymana
+        
     {
         // Wyświetl napis "GRA ZATRZYMANA"
         sf::Text pauseText;
